471a426dd301d88dadcb427872887d804eef61e2
Allan Leon <aleon.107@outlook.com>
2017-07-28 15:35:47 -0400

    Removed button noise

--- start-recording/start-recording.ino
+++ start-recording/start-recording.ino
@@ -1,22 +1,26 @@
-const int buttonPin = 2;
+const int buttonPin =10;
+int buttonState = 0;
+int lastButtonState = 0;
 
 void setup() 
 {
   Serial.begin(9600);         //Sets the data rate in bits per second (baud) for serial data transmission
-  pinMode(buttonPin, INPUT);        //button input
-  //pinMode(13, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);        //button input
+  pinMode(13, OUTPUT);
 }
 void loop()
 {
+  buttonState = digitalRead(buttonPin);
   sendButtonData();
-  //digitalWrite(13, digitalRead(buttonPin));
+  digitalWrite(13, buttonState);
+  lastButtonState = buttonState;
 }
 
 void sendButtonData()
 {
     //puts # before the values so our app knows what to do with the data
-    if (!digitalRead(buttonPin)) {
-        Serial.print("#");
+    if (buttonState != lastButtonState && digitalRead(buttonPin) == LOW) {
+        Serial.println("#");
     }
-    delay(10);        //added a delay to eliminate missed transmissions
+    delay(200);        //added a delay to eliminate missed transmissions
 }
